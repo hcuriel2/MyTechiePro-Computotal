edfa755c311e777b1a05bdc92c2d5eaedfe09ca8
slickwilfred <wondrik@bcit.ca>
2024-02-05 14:27:29 -0800

    Provided summaries and comments for the survey  directory (within controllers)

--- Backend/src/controllers/survey/survey.controller.ts
+++ Backend/src/controllers/survey/survey.controller.ts
@@ -1,24 +1,47 @@
+/*NOTES*/
+/*
+IF YOU MODIFY THE CODE: DOCUMENT THE CHANGE AND MENTION IT IN THE CHANGES SECTION
+
+
+Summary:
+This class is responsible for handling survey-related routes. It defines a base URL
+and a single route for retrieving all surveys.
+
+
+Unused Code:
+
+
+Clarification needed:
+
+
+
+Changes:
+
+
+*/
+
 import { Request, Response, Router } from "express";
 import Controller from "../../interfaces/controller.interface";
 import surveyModel from "../../models/survey/survey.model";
 
 
+// Class implements the Controller interface
 class SurveyController implements Controller {
-    public path = "/survey";
+    public path = "/survey"; // base url 
     public router = Router();
-    private survey = surveyModel;
+    private survey = surveyModel; // references the surveyModel
 
     constructor() {
         this.initializeRoutes();
     }
 
     private initializeRoutes() {
-        this.router.get(this.path, this.getAllSurveys);
+        this.router.get(this.path, this.getAllSurveys); // GET request: retrieves all surveys
     }
 
     private getAllSurveys = async (request: Request, response: Response) => {
-        const surveys = await this.survey.find();
-        response.send(surveys);
+        const surveys = await this.survey.find(); // retrieve all surveys  from the database
+        response.send(surveys);                   // send the retrieved surveys as a response
     };
 }
 
--- Backend/src/controllers/survey/survey.dto.ts
+++ Backend/src/controllers/survey/survey.dto.ts
@@ -1,6 +1,32 @@
+/*NOTES*/
+/*
+IF YOU MODIFY THE CODE: DOCUMENT THE CHANGE AND MENTION IT IN THE CHANGES SECTION
+
+
+Summary:
+These three classes serve as a data transfer objects (DTO) that are used for defining the structure and 
+validation rules creating a new survey. It appears that the surveyContents DTO is inserted
+into the category DTO, which is then used to create a new survey.
+
+
+Unused Code:
+
+
+
+Clarification needed:
+- why are there 3 DTO classes within this file?
+
+
+
+Changes:
+
+
+*/
+
 import { IsString, IsObject, ValidateNested } from "class-validator";
 import { Type } from "class-transformer";
 
+// DTO 
 class surveyContentsDto {
     public question: string;
     public key: string;
