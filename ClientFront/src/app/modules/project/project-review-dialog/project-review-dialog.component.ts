f9f561f41782bbf60eed2e700325b573c9ad1840
slickwilfred <wondrik@bcit.ca>
2024-03-05 01:01:43 -0800

    Injected the projectID into the review modal

--- ClientFront/src/app/modules/project/project-review-dialog/project-review-dialog.component.ts
+++ ClientFront/src/app/modules/project/project-review-dialog/project-review-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -11,13 +12,18 @@ export class ProjectReviewDialogComponent implements OnInit {
   reviewForm: FormGroup;
   hoverState = 0; // Set the initial star rating to 0
   clickedState = 0;
+  projectID: string; // Project ID
 
-  constructor(public dialogRef: MatDialogRef<ProjectReviewDialogComponent>) {}
+  constructor(public dialogRef: MatDialogRef<ProjectReviewDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { projectID: string }) {}
 
   ngOnInit(): void {
+    this.projectID = this.data.projectID;
+
     this.reviewForm = new FormGroup({
-      comment: new FormControl('', Validators.required),
+      review: new FormControl('', Validators.required),
       rating: new FormControl(0, [Validators.required, Validators.min(1), Validators.max(5)]),
+      projectID: new FormControl(this.projectID, Validators.required),
     });
   }
 
@@ -35,7 +41,31 @@ export class ProjectReviewDialogComponent implements OnInit {
   
   onSubmit(): void {
     if (this.reviewForm.valid) {
-      this.dialogRef.close(this.reviewForm.value);
+      const data = this.reviewForm.value;
+      console.log(data);
+      const url = 'http://localhost:3333/auth/reviews';
+
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+        
+      })
+      .then(data => {
+        console.log('Response: ', data);
+        this.dialogRef.close(this.reviewForm.value);
+      })
+      .catch(error => {
+        console.error('API Error: ', error);
+      })
     }
   }
 
