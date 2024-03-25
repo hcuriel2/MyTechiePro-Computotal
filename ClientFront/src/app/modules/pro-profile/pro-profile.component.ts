796f1d8b64305de0563f10c5f2963bdf1f9afe25
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2024-03-24 21:00:35 -0700

    created user profile to display user info such as name, location, skills, added css

--- ClientFront/src/app/modules/pro-profile/pro-profile.component.ts
+++ ClientFront/src/app/modules/pro-profile/pro-profile.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pro-profile.component.scss']
 })
 export class ProProfileComponent implements OnInit {
-
-  constructor() { }
+  userProfile: any;
 
   ngOnInit(): void {
+    this.loadUserProfile();
   }
+
+  private loadUserProfile(): void {
+    const userProfileString = localStorage.getItem('user');
+    if (userProfileString && userProfileString !== '{}') {
+      this.userProfile = JSON.parse(userProfileString);
+    } else {
+      console.log('No user profile information found in local storage.');
+      // Handle the case where there is no user information (e.g., redirect to login page)
+    }
+  }
+
+  usedHues: Set<number> = new Set();
+
+  getRandomHSLColor(): string {
+    let hue;
+    let attempts = 0;
+      do {
+      hue = Math.floor(Math.random() * 300); 
+      attempts++;
+      if (attempts > 50) {
+        console.warn("Too many attempts to find a unique hue.");
+        break;
+      }
+    } while (this.usedHues.has(hue));
+  
+    this.usedHues.add(hue);
+      return `hsl(${hue}, 100%, 20%)`;
+  }
+  
+
 
 }
