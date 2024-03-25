6ddf9286512e845a14bd6f9bf4be204704f56eda
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2024-03-24 18:05:07 -0700

    created routing and navigation for pro profile (added as a new module)

---
+++ ClientFront/src/app/modules/pro-profile/pro-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pro-profile',
+  templateUrl: './pro-profile.component.html',
+  styleUrls: ['./pro-profile.component.scss']
+})
+export class ProProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
