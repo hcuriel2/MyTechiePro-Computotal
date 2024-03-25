5175ea55894808f0788e5482070452d8b11c2f1e
Xinyang Li <xinyangli417@gmail.com>
2024-03-25 16:52:22 -0700

    Rearranged app components

--- ClientFront/src/app/modules/about/about.component.spec.ts
+++ ClientFront/src/app/modules/about/about.component.spec.ts
@@ -1,25 +1,73 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// @ts-ignore
+import { Component, OnInit } from "@angular/core";
+import { Options } from "ngx-google-places-autocomplete/objects/options/options";
 
-import { AboutComponent } from './about.component';
+@Component({
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
+})
+export class AboutComponent implements OnInit {
+  title = "google-places-autocomplete";
+  userAddress: string = "";
+  userLatitude: string = "";
+  userLongitude: string = "";
 
-describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
+  userCountry: string = "";
+  userPostal: string = "";
+  userCity: string = "";
+  userStreetAddress: string = "";
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
-    })
-    .compileComponents();
-  });
+  options: Options = {
+    bounds: undefined!,
+    fields: ["ALL"],
+    strictBounds: false,
+    types: ["address"],
+    origin: undefined!,
+    componentRestrictions: undefined!,
+  };
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AboutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  constructor() {}
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  ngOnInit(): void {}
+
+  handleAddressChange(address: any) {
+    let addressComponents = address.address_components;
+    console.log(addressComponents.length);
+
+    this.userAddress = address.formatted_address;
+    this.userLatitude = address.geometry.location.lat();
+    this.userLongitude = address.geometry.location.lng();
+
+    let streetNumber;
+    let streetName;
+
+    for (let i = 0; i < addressComponents.length; i++) {
+      let types = addressComponents[i].types;
+      for (let j = 0; j < types.length; j++) {
+        if (types[j] == "country") {
+          this.userCountry = addressComponents[i].long_name;
+        }
+        if (types[j] == "postal_code") {
+          this.userPostal = addressComponents[i].long_name;
+        }
+        if (types[j] == "locality") {
+          this.userCity = addressComponents[i].long_name;
+        }
+        if (types[j] == "street_number") {
+          streetNumber = addressComponents[i].long_name;
+        }
+        if (types[j] == "route") {
+          streetName = addressComponents[i].long_name;
+        }
+      }
+      this.userStreetAddress = streetNumber + " " + streetName;
+    }
+    console.log(this.userCountry);
+    console.log(this.userPostal);
+    console.log(this.userCity);
+    console.log(this.userStreetAddress);
+    console.log(address);
+    // this.userCountry = address.geometry.location.country()
+  }
+}
