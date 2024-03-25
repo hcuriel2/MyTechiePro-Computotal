70c9fc796fae58c62b8ed90e347520bea58ccce5
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2024-03-24 21:35:41 -0700

    css for pro profile page, as well as footer -specific color if pro user is logged in

--- ClientFront/src/app/modules/pro-profile/pro-profile.component.ts
+++ ClientFront/src/app/modules/pro-profile/pro-profile.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProProfileComponent implements OnInit {
   userProfile: any;
+  availableColors = [
+    'rgb(1, 38, 81)',
+    'rgb(16, 63, 116)',
+    'rgb(28, 72, 121)',
+    'rgb(38, 85, 138)',
+    'rgb(60, 103, 151)',
+    'rgb(59, 120, 188)',
+    'rgb(74, 130, 194)'
+  ];
 
   ngOnInit(): void {
     this.loadUserProfile();
@@ -18,28 +27,10 @@ export class ProProfileComponent implements OnInit {
       this.userProfile = JSON.parse(userProfileString);
     } else {
       console.log('No user profile information found in local storage.');
-      // Handle the case where there is no user information (e.g., redirect to login page)
     }
   }
 
-  usedHues: Set<number> = new Set();
-
-  getRandomHSLColor(): string {
-    let hue;
-    let attempts = 0;
-      do {
-      hue = Math.floor(Math.random() * 300); 
-      attempts++;
-      if (attempts > 50) {
-        console.warn("Too many attempts to find a unique hue.");
-        break;
-      }
-    } while (this.usedHues.has(hue));
-  
-    this.usedHues.add(hue);
-      return `hsl(${hue}, 100%, 20%)`;
+  getSkillColor(index: number): string {
+    return this.availableColors[index % this.availableColors.length];
   }
-  
-
-
 }
