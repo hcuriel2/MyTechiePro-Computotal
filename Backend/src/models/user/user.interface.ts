3032b6467cbfdfd2bf9cef36100b5120e712e313
slickwilfred <wondrik@bcit.ca>
2024-03-28 14:01:42 -0700

    Modified user interface - this was needed to be done to properly fetch the full user information, as the previous version would not allow perforance to be accessed which threw errors. This needed to be changed to work with the more secure httpOnly cookies

--- Backend/src/models/user/user.interface.ts
+++ Backend/src/models/user/user.interface.ts
@@ -1,3 +1,5 @@
+/*
+// Original implementation
 interface User {
     _id: string;
     firstName: string;
@@ -16,6 +18,53 @@ interface User {
     company : string;
     approved: Boolean;
     verified: Boolean;
+}
+
+export default User;
+*/
+
+interface Address {
+    city: string;
+    country?: string;
+    street: string;
+    postalCode?: string;
+    lat: number;
+    lng: number;
+    placeid?: string;
+}
+
+interface Performance {
+    rating: number;
+    feedback: string;
+    clientName: string;
+    service: string;
+}
+
+interface User {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    email: string;
+    password: string;
+    userType: 'Admin' | 'Client' | 'Professional';
+    address?: Address;
+    company?: string;
+    proStatus?: 'Busy' | 'Active';
+    skills?: string[];
+    unitPrice?: number;
+    ratingSum?: number;
+    ratingCount?: number;
+    rating?: number;
+    unitType?: 'Hour' | 'Solution' | 'Flat fee';
+    bio?: string;
+    inquiry?: string;
+    performance?: Performance[];
+    website?: string;
+    secret?: string;
+    tempSecret?: string;
+    approved: boolean;
+    verified: boolean;
 }
 
 export default User;
