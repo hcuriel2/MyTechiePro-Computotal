4ea49bec183c222e245471576f27e93d9c5b296a
slickwilfred <wondrik@bcit.ca>
2024-04-01 21:28:28 -0700

    Added logs and error checks on checksession and signin functions

--- ClientFront/src/app/shared/services/auth.service.ts
+++ ClientFront/src/app/shared/services/auth.service.ts
@@ -27,20 +27,27 @@ export class AuthService {
         return this.userSubject.value;
     }
 
+    // Sets the current User's value
+    public setUserValue(user: User | null): void {
+        this.userSubject.next(user);
+    }
+
     // Register a new User
     public registerUser(user: User): Observable<User> {
         return this.httpClient.post<User>(`${this.API_URL}/register`, user, { withCredentials: true });  
     }
 
-// Signs in a User
+    // Signs in a User
     // Assigns the User's values to the UserSubject
     // It's then accessible through the 'this.user' value
     public signIn(user: User): Observable<User> {
+        console.log('authservice: attempting signin for', user)
          return this.httpClient.post<User>(`${this.API_URL}/login`, user, { withCredentials: true }).pipe(
           tap((user: User) => {
             this.userSubject.next(user);
+            console.log('SIGN IN function', user);
           }),
-          switchMap(() => this.checkSession())
+          //switchMap(() => this.checkSession())
         );
     }
 
@@ -53,7 +60,6 @@ export class AuthService {
     // Signs out the User
     // Updates the userSubject null, so no User values remain
     public signOut(): Observable<any> {
-        console.log('LOGOUT AUTHSERVICE')
         return this.httpClient.post(`${this.API_URL}/logout`, {}, { withCredentials: true }).pipe(
             tap(() => {
                 this.userSubject.next(null);
@@ -67,12 +73,13 @@ export class AuthService {
     // Retrieves the User information from the database
     // Passes it into the userSubject - which allows the UI to be applied from the User values
     public checkSession(): Observable<User> {
-
         return this.httpClient.get<User>(`${this.API_URL}/checkSession`, { withCredentials: true }).pipe(
             tap((user: User) => {
-                this.userSubject.next(user);
-                console.log('auth service user', this.userSubject);
-            })
+                this.userSubject.next(user)
+            }, error => {
+                console.log('auth service no session found or error', error);
+            }            
+            )
         );
     }
       
@@ -80,7 +87,8 @@ export class AuthService {
     // Updates the User's information on the 'settings' page
     // All changes will update the User entry in the database
     updateUserSettings(userId: string, updates: any): Observable<any> {
-        return this.httpClient.patch(`${this.API_URL}/settings/${userId}`, updates);
+        console.log(userId)
+        return this.httpClient.patch(`${this.API_URL}/settings/${userId}`, updates, { withCredentials: true });
     }
 
     getUserInfo(): Observable<any> {
