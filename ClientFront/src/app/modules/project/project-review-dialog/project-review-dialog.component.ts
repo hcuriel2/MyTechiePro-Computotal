63c1d960940e52b058d1ef3d4c69471c740c6f81
Flora Deng <hdeng24@my.bcit.ca>
2025-01-26 17:35:19 -0800

    Chore: implement successMessage, errorMessage

--- ClientFront/src/app/modules/project/project-review-dialog/project-review-dialog.component.ts
+++ ClientFront/src/app/modules/project/project-review-dialog/project-review-dialog.component.ts
@@ -1,9 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+// import { Inject } from '@angular/core';
+// import { FormControl, FormGroup, Validators } from '@angular/forms';
+// import { environment } from 'src/environments/environment';
+
+
+// @Component({
+//   selector: 'app-project-review-dialog',
+//   templateUrl: './project-review-dialog.component.html',
+//   styleUrls: ['./project-review-dialog.component.scss'],
+// })
+// export class ProjectReviewDialogComponent implements OnInit {
+//   private readonly API_URL: string;
+//   reviewForm: FormGroup;
+//   hoverState = 0; // Set the initial star rating to 0
+//   clickedState = 0;
+//   projectID: string; // Project ID
+
+//   constructor(public dialogRef: MatDialogRef<ProjectReviewDialogComponent>,
+//     @Inject(MAT_DIALOG_DATA) public data: { projectID: string }) {
+//       this.API_URL = `${environment.apiEndpoint}/projects`;
+
+//     }
+
+//   ngOnInit(): void {
+//     this.projectID = this.data.projectID;
+
+//     this.reviewForm = new FormGroup({
+//       review: new FormControl('', Validators.required),
+//       rating: new FormControl(0, [Validators.required, Validators.min(1), Validators.max(5)]),
+//       projectID: new FormControl(this.projectID, Validators.required),
+//     });
+//   }
+
+//   onRate(rating: number): void {
+//     this.reviewForm.controls['rating'].setValue(rating);
+//   }
+
+//   hoveredOver(rate: number): void {
+//     this.hoverState = rate;
+//   }
+
+//   hoveredLeave(): void {
+//     this.hoverState = 0;
+//   }
+  
+//   onSubmit(): void {
+//     if (this.reviewForm.valid) {
+//       const data = this.reviewForm.value;
+      
+
+//       fetch(`${this.API_URL}/projectReview`,  {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+          
+//         },
+//         body: JSON.stringify(data),
+//         credentials: 'include'
+//       })
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         return response.json();
+        
+//       })
+//       .then(data => {
+        
+//         this.dialogRef.close(this.reviewForm.value);
+//       })
+//       .catch(error => {
+//         console.error('API Error: ', error);
+//       })
+//     }
+//   }
+
+//   onCancel(): void {
+//     this.dialogRef.close();
+//   }
+
+// }
+
+import { Component, OnInit } from '@angular/core'; 
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
-
 
 @Component({
   selector: 'app-project-review-dialog',
@@ -16,19 +99,26 @@ export class ProjectReviewDialogComponent implements OnInit {
   hoverState = 0; // Set the initial star rating to 0
   clickedState = 0;
   projectID: string; // Project ID
+  successMessage: string = ''; 
+  errorMessage: string = '';
 
-  constructor(public dialogRef: MatDialogRef<ProjectReviewDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { projectID: string }) {
-      this.API_URL = `${environment.apiEndpoint}/projects`;
-
-    }
+  constructor(
+    public dialogRef: MatDialogRef<ProjectReviewDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { projectID: string }
+  ) {
+    this.API_URL = `${environment.apiEndpoint}/projects`;
+  }
 
   ngOnInit(): void {
     this.projectID = this.data.projectID;
 
     this.reviewForm = new FormGroup({
       review: new FormControl('', Validators.required),
-      rating: new FormControl(0, [Validators.required, Validators.min(1), Validators.max(5)]),
+      rating: new FormControl(0, [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(5),
+      ]),
       projectID: new FormControl(this.projectID, Validators.required),
     });
   }
@@ -44,42 +134,10 @@ export class ProjectReviewDialogComponent implements OnInit {
   hoveredLeave(): void {
     this.hoverState = 0;
   }
-  
-  onSubmit(): void {
-    if (this.reviewForm.valid) {
-      const data = this.reviewForm.value;
-      
 
-      fetch(`${this.API_URL}/projectReview`,  {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          
-        },
-        body: JSON.stringify(data),
-        credentials: 'include'
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-        
-      })
-      .then(data => {
-        
-        this.dialogRef.close(this.reviewForm.value);
-      })
-      .catch(error => {
-        console.error('API Error: ', error);
-      })
-    }
-  }
+  
 
   onCancel(): void {
     this.dialogRef.close();
   }
-
-
-
 }
