7b33808ea2bf68bf86c00f7b74ff678567f48c5e
Echo Wang <ywang623@my.bcit.ca>
2025-03-15 00:03:47 -0700

    Adjusted logic so when there's more than 1 page of content, the footer only appears when you scrool to the bottom

--- ClientFront/src/app/app.component.ts
+++ ClientFront/src/app/app.component.ts
@@ -5,30 +5,30 @@ import {
   AfterViewInit,
   OnDestroy,
   OnInit,
-} from "@angular/core";
-import { TranslateService } from "@ngx-translate/core";
-import { Router } from "@angular/router";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import { User } from "./shared/models/user";
-import { MatSnackBar } from "@angular/material/snack-bar";
-import { first } from "rxjs/operators";
-import { AuthService } from "./shared/services/auth.service";
-import { SignInComponent } from "./modules/sign-in/sign-in.component";
-import { Options } from "ngx-google-places-autocomplete/objects/options/options";
-import { Message } from "./shared/models/message";
-import { GoogleMapsService } from "./shared/services/google-maps.service";
+} from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { User } from './shared/models/user';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { first } from 'rxjs/operators';
+import { AuthService } from './shared/services/auth.service';
+import { SignInComponent } from './modules/sign-in/sign-in.component';
+import { Options } from 'ngx-google-places-autocomplete/objects/options/options';
+import { Message } from './shared/models/message';
+import { GoogleMapsService } from './shared/services/google-maps.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   //changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit, OnDestroy {
   public user: User | null | undefined;
-  userAddress: string = "";
-  userLatitude: string = "";
-  userLongitude: string = "";
+  userAddress: string = '';
+  userLatitude: string = '';
+  userLongitude: string = '';
   isProfessional: boolean = false;
   isMenuOpen: boolean = false;
   currentYear: number = new Date().getFullYear();
@@ -42,22 +42,22 @@ export class AppComponent implements OnInit, OnDestroy {
     private translateService: TranslateService,
     private googleMapsService: GoogleMapsService
   ) {
-    translateService.setDefaultLang("en-us");
-    translateService.use("en-us");
+    translateService.setDefaultLang('en-us');
+    translateService.use('en-us');
 
     this.authService.user.subscribe(
       (u: any) =>
         (this.user = u === null || Object.keys(u).length === 0 ? null : u)
     );
   }
-  title = "computer-guy-frontend";
+  title = 'computer-guy-frontend';
 
   public ngOnDestroy(): void {}
 
   public ngOnInit(): void {
     this.googleMapsService
       .loadGoogleMapsScript()
-      .catch((error) => console.error("Error loading Google Maps:", error));
+      .catch((error) => console.error('Error loading Google Maps:', error));
     if (!this.authService.isSessionChecked) {
       this.authService.checkSession().subscribe({
         next: (user) => {
@@ -68,7 +68,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
     this.authService.user.subscribe((user) => {
       if (user) {
-        this.isProfessional = user.userType === "Professional";
+        this.isProfessional = user.userType === 'Professional';
         this.updateUIBasedOnUser(user);
       } else {
         this.isProfessional = false;
@@ -78,29 +78,29 @@ export class AppComponent implements OnInit, OnDestroy {
 
   private updateUIBasedOnUser(user: User | null): void {
     // Checks the userType to adjust UI elements accordingly
-    const signupBtn = document.getElementById("app-menu-signup-btn");
-    const joinBtn = document.getElementById("app-menu-join-btn");
-    const projectBtn = document.getElementById("app-menu-project-btn");
-    const userInfo = document.getElementById("app-menu-user-info");
-    const toolbar = document.getElementById("app-toolbar");
-    const footer = document.getElementById("footer");
+    const signupBtn = document.getElementById('app-menu-signup-btn');
+    const joinBtn = document.getElementById('app-menu-join-btn');
+    const projectBtn = document.getElementById('app-menu-project-btn');
+    const userInfo = document.getElementById('app-menu-user-info');
+    const toolbar = document.getElementById('app-toolbar');
+    const footer = document.getElementById('footer');
 
     if (signupBtn) {
-      signupBtn.style.display = "none";
+      signupBtn.style.display = 'none';
     }
     if (joinBtn) {
-      joinBtn.style.display = "none";
+      joinBtn.style.display = 'none';
     }
     if (projectBtn) {
-      projectBtn.style.display = "block";
+      projectBtn.style.display = 'block';
     }
     if (userInfo) {
-      userInfo.style.display = "block";
+      userInfo.style.display = 'block';
     }
     if (toolbar && footer) {
-      if (this.user?.userType === "Professional") {
-        toolbar.style.backgroundColor = "#ef0078";
-        footer.style.background = "#ef0078";
+      if (this.user?.userType === 'Professional') {
+        toolbar.style.backgroundColor = '#ef0078';
+        footer.style.background = '#ef0078';
       }
     }
 
@@ -141,52 +141,52 @@ export class AppComponent implements OnInit, OnDestroy {
         this.authService.setUserValue(null);
       },
     });
-    setTimeout(()=>{
-      this.router.navigateByUrl("/").then(() => {
+    setTimeout(() => {
+      this.router.navigateByUrl('/').then(() => {
         window.location.reload();
       });
-    }, 500)
+    }, 500);
   }
 
   // Navigates to the Home page
   public routeToHomePage(): void {
-    this.router.navigateByUrl("/");
+    this.router.navigateByUrl('/');
   }
 
   // Handles page routing
   // Dependent on the userType value of the current User
   public routeBasedOnUser(): void {
-    if (this.user?.userType === "Professional") {
-      this.router.navigateByUrl("/projects");
+    if (this.user?.userType === 'Professional') {
+      this.router.navigateByUrl('/projects');
     } else {
-      this.router.navigateByUrl("/");
+      this.router.navigateByUrl('/');
     }
   }
 
   // Redirect to about page
   public routeToAboutPage(): void {
-    this.router.navigateByUrl("/about");
+    this.router.navigateByUrl('/about');
   }
 
   // Redirect to contact page
   public routeToContactUsPage(): void {
-    this.router.navigateByUrl("/contact-us");
+    this.router.navigateByUrl('/contact-us');
   }
 
   // Redirect to project list page
   public renderProjectsList(): void {
-    this.router.navigate(["/projects"]);
+    this.router.navigate(['/projects']);
   }
 
   // Redirect to sign up page
   public signUp(): void {
     const currentUrl = this.router.url;
-    const isOnSignUpPage = currentUrl.includes("sign-up");
+    const isOnSignUpPage = currentUrl.includes('sign-up');
     const urlParams = new URLSearchParams(window.location.search);
-    const isProSignup = urlParams.get("forPro") === "true";
+    const isProSignup = urlParams.get('forPro') === 'true';
 
     if (!isOnSignUpPage || isProSignup) {
-      window.location.href = "/sign-up?forPro=false";
+      window.location.href = '/sign-up?forPro=false';
     } else {
       window.location.reload();
     }
@@ -195,10 +195,10 @@ export class AppComponent implements OnInit, OnDestroy {
   // Redirect to sign up page for professional
   public joinPro(): void {
     const urlParams = new URLSearchParams(window.location.search);
-    const isProSignup = urlParams.get("forPro") === "true";
+    const isProSignup = urlParams.get('forPro') === 'true';
 
     if (!isProSignup) {
-      window.location.href = "/sign-up?forPro=true";
+      window.location.href = '/sign-up?forPro=true';
     } else {
       window.location.reload();
     }
@@ -206,11 +206,11 @@ export class AppComponent implements OnInit, OnDestroy {
 
   // Navigates to the User's settings page
   public settings(): void {
-    this.router.navigateByUrl("/settings");
+    this.router.navigateByUrl('/settings');
   }
 
   // Navigates to Professional's profile
   public navigateToProProfile(): void {
-    this.router.navigateByUrl("/pro-profile");
+    this.router.navigateByUrl('/pro-profile');
   }
 }
